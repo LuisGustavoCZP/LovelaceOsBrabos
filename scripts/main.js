4b294a11eb78adc0592f46eaa6b6d0545b603484
LuisGustavoCZP <luisgustavoczp@gmail.com>
2021-11-17 18:34:00 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,28 @@
-var foo = 1;
-
-console.log(foo);
-
-function do_something() {
-    foo = "hello"; // Isso funciona.
-    console.log(foo);
+class Aluno {
+    constructor(nome, turma, grupo){
+        this.nome = nome;
+        this.turma = turma;
+        this.grupo = grupo;
+    }
 }
 
-do_something();
+var Aluno2
+{
+    let nome = "Felype"
+    let turma = "Lovelace"
+    let grupo = 1;
+};
+
+const alunosFixos = 
+[
+    new Aluno("Luis", "Lovelace", 2),
+    new Aluno("Neto", "Lovelace", 2)
+];
+
+function ChecarAlunos() {
+    for (aluno in alunosFixos){
+        console.log(alunosFixos[aluno]);
+    }
+}
+
+ChecarAlunos();
